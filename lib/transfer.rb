70474b52f4756d4e3627b463892f682cc6cd4d73
Milan Mody <milanm1120@gmail.com>
2021-03-21 15:57:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,7 @@
 class Transfer
 
+  attr_accessor :transfer_sender, :transfer_reciever, :amount
+
   def initialize(transfer_sender, transfer_reciever, amount)
     @sender = transfer_sender
     @reciever = transfer_reciever
