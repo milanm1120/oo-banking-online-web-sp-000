d9e1e896dc49d57d11a0937b59fab29a52e671e4
Milan Mody <milanm1120@gmail.com>
2021-03-21 16:21:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,14 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance > @amount && self.status == "pending"
+    if
+      valid? && sender.balance > @amount && self.status == "pending"
       sender.balance -= @amount
       receiver.balance += @amount
       self.status = "complete"
+    else
+      reject_transfer
+    end
   end
 
 end
