b7652f88cfd67dc0d267f6fd43fa2beb47a4d03d
Milan Mody <milanm1120@gmail.com>
2021-03-21 15:32:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name, balance = "1000", status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
 end
